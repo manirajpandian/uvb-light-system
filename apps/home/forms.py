8d6683f52699fd3281c3ad71d70a9e8b79578dab
manirajpandian <maniraj.pandian@mosaique.link>
2023-11-10 12:17:10 +0530

    unnesscary files removing

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -5,11 +5,11 @@ from .models import users
 class UserAddForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(UserAddForm, self).__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs.update({'placeholder': 'Username'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['name'].widget.attrs.update({'placeholder': '⽒名'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'メール'})
         self.fields['affiliation'].widget.attrs.update({'placeholder': 'Affiliation'})
-        self.fields['address'].widget.attrs.update({'placeholder': 'Address'})
-        self.fields['houseName'].widget.attrs.update({'placeholder': 'House Name'})
+        self.fields['address'].widget.attrs.update({'placeholder': '住所'})
+        self.fields['houseName'].widget.attrs.update({'placeholder': 'ハウス名'})
         self.fields['plantName'].widget.attrs.update({'placeholder': 'Plant Name'})
         CHOICES = [('option1', '権限を選択してください'), ('管理者', '管理者'), ('ユーザ', 'ユーザ')]
         self.fields['role'] = forms.ChoiceField(choices=CHOICES, initial='option1', widget=forms.Select(attrs={'class': 'form-control'}), label="")
