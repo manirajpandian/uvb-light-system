c5ebb5ca530fd3524a782c84d04f682c32a680ec
manirajpandian <maniraj.pandian@mosaique.link>
2023-12-08 15:03:54 +0530

    settings file change

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'postgres_13',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
